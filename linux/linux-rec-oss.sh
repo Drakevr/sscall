c96093d28616a2b9cc55f14ac10f98917e962fba
Dimitris Papastamos <dp@opensource.wolfsonmicro.com>
2012-06-17 17:47:51 +0100

    sscall: Update recording scripts for the new default srate change
    Signed-off-by: Dimitris Papastamos <dp@opensource.wolfsonmicro.com>

--- linux/linux-rec-oss.sh
+++ linux/linux-rec-oss.sh
@@ -3,4 +3,4 @@
 # Sample Linux OSS recording script
 # Pipe this over to sscall
 
-ossrecord -s 8000 -c 1 -b 16 -
+ossrecord -s 16000 -c 1 -b 16 -
--- obsd/obsd-rec.sh
+++ obsd/obsd-rec.sh
@@ -3,4 +3,4 @@
 # Sample OpenBSD recording script
 # Pipe this over to sscall
 
-aucat -r 8000 -C0:0 -e s16le -o -
+aucat -r 16000 -C0:0 -e s16le -o -
